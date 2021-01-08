9187fdda8a4e28e061d4cde0b50239aa4d3c1f93
Magesh Kuppan <tkmagesh77@gmail.com>
2021-01-08 13:18:43 +0530

    server communication logic

--- bug-tracker-app/src/app/app.component.ts
+++ bug-tracker-app/src/app/app.component.ts
@@ -17,8 +17,37 @@ export class AppComponent {
   }
 
   ngOnInit() {
-    this.http
+    //getting the data
+    /* this.http
       .get('http://localhost:3000/bugs')
-      .subscribe(response => console.table(response));
+      .subscribe(response => console.table(response)); */
+
+    //to add a new bug
+      /* const newBugData = {
+        id : 0,
+        name : 'Test Bug for JD',
+        isClosed : false,
+        createdAt : new Date()
+      }
+      this.http
+        .post('http://localhost:3000/bugs', newBugData)
+        .subscribe(newBug => console.log(newBug)) */
+
+      //updating an existing bug
+
+      /* const updatedBug = {
+        id : 4,
+        name : 'Test Bug for JD - updated',
+        isClosed : true,
+        createdAt : new Date()
+      }
+      this.http
+        .put(`http://localhost:3000/bugs/${updatedBug.id}`, updatedBug)
+        .subscribe(newBug => console.log(newBug)) */
+
+      //removing an existing bug
+      /* this.http
+        .delete('http://localhost:3000/bugs/4')
+        .subscribe(response => console.log('bug removed')); */
   }
 }
