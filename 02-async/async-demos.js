f9333354727376bd85a73adbd5e4f22a61b367f3
Magesh Kuppan <tkmagesh77@gmail.com>
2021-01-08 11:17:36 +0530

    Promises & async await

--- 02-async/async-demos.js
+++ 02-async/async-demos.js
@@ -33,4 +33,34 @@
 
     window['addAsyncClient'] = addAsyncClient;
 
+    function addAsyncPromise(x,y){
+        console.log(`   [@service] processing ${x} and ${y}`);
+        const p = new Promise(function(resolveFn, rejectFn){
+            setTimeout(function(){
+                const result = x + y;
+                console.log(`   [@service] returning result`);
+                resolveFn(result);
+            }, 4000);
+        });
+        return p;
+    }
+
+    /* 
+    function addAsyncPromiseClient(x,y){
+        console.log(`[@client] triggering the service`);
+        var p = addAsyncPromise(100,200);
+        p.then(function(result){
+            console.log(`[@client] result = ${result}`);
+        });
+    } 
+    */
+
+    async function addAsyncPromiseClient(x,y){
+        console.log(`[@client] triggering the service`);
+        var result = await addAsyncPromise(100,200);
+        console.log(`[@client] result = ${result}`);
+    }
+
+    window['addAsyncPromiseClient'] = addAsyncPromiseClient;
+
 })();
